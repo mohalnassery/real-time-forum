24c59459b99aac5ac9a6a92a94ee486139633d50
mohalnassery <moh.alnassery@gmail.com>
2024-08-03 20:12:38 +0300

    syntax and import error

--- frontend/js/components/chat/chat.js
+++ frontend/js/components/chat/chat.js
@@ -1,4 +1,4 @@
-import { getMessages, sendMessage, getUsers, sendTyping, sendStopTyping } from '../websocket/websocket.js';
+import { getMessages, sendMessage, getUsers, sendTyping, sendStopTyping, throttle } from '../websocket/websocket.js';
 
 let throttler = false;
 let scrollListener = null;
